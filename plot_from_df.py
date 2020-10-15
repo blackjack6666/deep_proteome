29f5052d409067f5781c1bf7bafc1cee9c0c56bd
blackjack <xshao8@uic.edu>
2020-10-14 17:06:38 -0700

    10_5

--- plot_from_df.py
+++ plot_from_df.py
@@ -7,7 +7,7 @@ from calculations_and_plot import length_KR_ratio, specific_aa_length_ratio
 
 fasta_path = 'D:/data/proteome_fasta/uniprot-proteome_UP000005640.fasta'
 protein_dict = fasta_reader2(fasta_path)
-kr_len_ratio = specific_aa_length_ratio(protein_dict,spec_aa='FWY')
+kr_len_ratio = specific_aa_length_ratio(protein_dict,spec_aa='KR')
 
 
 protein_len_dict = {each:len(protein_dict[each]) for each in protein_dict}
@@ -17,12 +17,13 @@ pd.set_option('display.max_columns', None)
 df = pd.read_excel("D:/data/deep_proteome/9_20_identified_cov_total.xlsx")
 
 
-df['protein_num'] = range(len(df))
+# df['protein_num'] = range(len(df))
 df = df.set_index('Unnamed: 0')
 protein_list = df.index.tolist()
 
-kr_len_ratio_list = [kr_len_ratio[each] for each in protein_list]
-df['kr_len_ratio'] = kr_len_ratio_list
+
+# kr_len_ratio_list = [kr_len_ratio[each] for each in protein_list]
+# df['kr_len_ratio'] = kr_len_ratio_list
 # kr length ratio plot
 
 # plt.hist(kr_len_ratio_list,bins=100,color='black')
@@ -34,8 +35,8 @@ df['kr_len_ratio'] = kr_len_ratio_list
 # protein_len_list = [protein_len_dict[each] for each in protein_list]
 # df['protein_len'] = protein_len_list
 
-df = df.sort_values('kr_len_ratio')
-print (df[-100:])
+# df = df.sort_values('kr_len_ratio')
+
 # # df_split = np.split(df,[200],axis=0)[0]
 # # df_split = df_split[df_split.columns[:-1]]
 # # print (df_split.head())
@@ -45,9 +46,12 @@ fig, ax = plt.subplots(1,1, figsize=(40,20))
 #     print (each_cond)
 #     ax.plot(df_split['protein_num'],df_split[each_cond], '-', c=np.random.rand(3,), label=each_cond)
 
-g = sns.heatmap(df[df.columns[:-2]],ax=ax,yticklabels=False)
+g = sns.violinplot(data=df.iloc[:,:18], color="skyblue", width=0.8)
+
+# g = sns.heatmap(df[df.columns[:-2]],ax=ax,yticklabels=False)
 #
 plt.xticks(rotation=60)
+plt.ylabel('coverage in %')
 plt.show()
 
 # # charge plot
