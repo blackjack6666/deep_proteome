f5851bfa5bb0b86f713d74a669e4e6c0ddb6bf76
blackjack <xshao8@uic.edu>
2020-11-17 18:07:02 -0800

    keras

--- bioplex_ana.py
+++ bioplex_ana.py
@@ -1,8 +1,26 @@
 import pickle as ppp
 import matplotlib.pyplot as plt
-bioplex_cov_dict = ppp.load(open('bioplex_protein_coverage_dict.p','rb'))
-coverage_list = [each for each in bioplex_cov_dict.values()]
-plt.hist(coverage_list,bins=100,color='black')
+import statistics
+import pandas as pd
+
+# bioplex_cov_dict = ppp.load(open('bioplex_protein_coverage_dict.p','rb'))
+# coverage_list = [each for each in bioplex_cov_dict.values()]
+# print (statistics.mean(coverage_list),statistics.median(coverage_list))
+# print (len(coverage_list))
+# plt.hist(coverage_list,bins=100,color='black')
+# plt.xlabel('sequence coverage %')
+# plt.ylabel('frequency')
+# plt.show()
+
+# matrisome db data
+
+matrisome_cov_csv = 'D:/data/Naba_deep_matrisome/seq_cov_matrisomedb.csv'
+
+df = pd.read_csv(matrisome_cov_csv)
+cov_list = df['cov'].tolist()
+print (len(cov_list))
+print (statistics.mean(cov_list),statistics.median(cov_list))
+plt.hist(cov_list,bins=100,color='black')
 plt.xlabel('sequence coverage %')
 plt.ylabel('frequency')
 plt.show()
