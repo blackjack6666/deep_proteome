2f7e082a2628064ba1e8704e9276004c589bab17
blackjack <xshao8@uic.edu>
2020-11-18 16:22:27 -0800

    pytorch

---
+++ pytorch_imple.py
@@ -0,0 +1,125 @@
+import pickle as ppp
+from stat_models import matrix_target, train_test_data_split
+from parameters import custom_ohe
+from torch.utils.data import TensorDataset, DataLoader
+import torch
+from torch import nn
+import numpy as np
+from torch.optim.lr_scheduler import _LRScheduler
+from torch.nn import functional as F
+
+
+class LSTMClassifier(nn.Module):
+    """Very simple implementation of LSTM-based time-series classifier."""
+
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.rnn = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.batch_size = None
+        self.hidden = None
+
+    def forward(self, x):
+        h0, c0 = self.init_hidden(x)
+        out, (hn, cn) = self.rnn(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+    def init_hidden(self, x):
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim)
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim)
+        return [t.cuda() for t in (h0, c0)]
+
+
+class CyclicLR(_LRScheduler):
+
+    def __init__(self, optimizer, schedule, last_epoch=-1):
+        assert callable(schedule)
+        self.schedule = schedule
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        return [self.schedule(self.last_epoch, lr) for lr in self.base_lrs]
+
+
+def cosine(t_max, eta_min=0):
+    def scheduler(epoch, base_lr):
+        t = epoch % t_max
+        return eta_min + (base_lr - eta_min) * (1 + np.cos(np.pi * t / t_max)) / 2
+
+    return scheduler
+
+t_37C_240min_dict = ppp.load(open('D:/data/non_specific_search/ecoli_non_specific_search_poly_dict.p','rb'))
+matrix,target = matrix_target(t_37C_240min_dict)
+matrix = custom_ohe(matrix)
+X_train, X_test, target_train, target_test = train_test_data_split(matrix,target)
+
+# convert numpy array data into tensor
+X_train_tensor,target_train_tensor = torch.from_numpy(X_train), torch.from_numpy(target_train)
+X_test_tensor, target_test_tensor = torch.from_numpy(X_test), torch.from_numpy(target_test)
+
+train_tensor = TensorDataset(X_train_tensor,target_train_tensor)
+train_loader = DataLoader(dataset=train_tensor, batch_size=64, shuffle=True)
+test_tensor = TensorDataset(X_test_tensor,target_test_tensor)
+test_loader = DataLoader(dataset=test_tensor, batch_size=64, shuffle=True)
+
+
+input_dim = 10
+hidden_dim = 256
+layer_dim = 3
+output_dim = 9
+seq_dim = 128
+
+lr = 0.0005
+n_epochs = 1000
+iterations_per_epoch = len(train_loader)
+best_acc = 0
+patience, trials = 100, 0
+
+model = LSTMClassifier(input_dim, hidden_dim, layer_dim, output_dim)
+model = model.cuda()
+criterion = nn.CrossEntropyLoss()
+opt = torch.optim.RMSprop(model.parameters(), lr=lr)
+sched = CyclicLR(opt, cosine(t_max=iterations_per_epoch * 2, eta_min=lr/100))
+
+print('Start model training')
+
+for epoch in range(1, n_epochs + 1):
+
+    for i, (x_batch, y_batch) in enumerate(train_loader):
+        model.train()
+        x_batch = x_batch.cuda()
+        y_batch = y_batch.cuda()
+        sched.step()
+        opt.zero_grad()
+        out = model(x_batch)
+        loss = criterion(out, y_batch)
+        loss.backward()
+        opt.step()
+
+    model.eval()
+    correct, total = 0, 0
+    for x_val, y_val in test_loader :
+        x_val, y_val = [t.cuda() for t in (x_val, y_val)]
+        out = model(x_val)
+        preds = F.log_softmax(out, dim=1).argmax(dim=1)
+        total += y_val.size(0)
+        correct += (preds == y_val).sum().item()
+
+    acc = correct / total
+
+    if epoch % 5 == 0:
+        print(f'Epoch: {epoch:3d}. Loss: {loss.item():.4f}. Acc.: {acc:2.2%}')
+
+    if acc > best_acc:
+        trials = 0
+        best_acc = acc
+        torch.save(model.state_dict(), 'best.pth')
+        print(f'Epoch {epoch} best model saved with accuracy: {best_acc:2.2%}')
+    else:
+        trials += 1
+        if trials >= patience:
+            print(f'Early stopping on epoch {epoch}')
+            break
