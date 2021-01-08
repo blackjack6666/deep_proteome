27b95348a3231a9b5eaf21065b40ea48412d51c2
blackjack <xshao8@uic.edu>
2021-01-08 16:03:09 -0600

    naba

--- fasta_reverse.py
+++ fasta_reverse.py
@@ -4,17 +4,18 @@ Generate reverse protein sequence and output to a fasta file
 from protein_coverage import fasta_reader, read_description
 
 
-def fasta_reverse_generator(uniprot_id_list, fasta_file_in, fasta_file_out):
+def fasta_reverse_generator(fasta_file_in, fasta_file_out):
     print ('reverse_algorithm = null, or other protease')
     # read protein sequence into dic
     protein_dict = fasta_reader(fasta_file_in)
+
 
     # read description into dic
     ID_descrip_dict = read_description(fasta_file_in)  # dictionary format: {'ID': ('sp'or'tr', description)}
 
     # write id and reverse sequence into fasta_file_out
     with open(fasta_file_out, 'w', newline='\n') as file_open:
-        for id in uniprot_id_list:
+        for id in protein_dict:
             seq = protein_dict[id]
             rev_seq = protein_dict[id][::-1]
 
@@ -37,4 +38,4 @@ if __name__=='__main__':
     proteome_dict = fasta_reader(fasta_file_in)
     id_list = [k for k in proteome_dict]
 
-    fasta_reverse_generator(id_list,fasta_file_in,'D:/data/Naba_deep_matrisome/uniprot-proteome_UP000000589_mouse_human_SNED1_rev.fasta')
+    fasta_reverse_generator(fasta_file_in,'D:/data/Naba_deep_matrisome/uniprot-proteome_UP000000589_mouse_human_SNED1_rev.fasta')
