75dae8efdf843817151eae51ffad26ef78cc25a8
Xinhao <xshao8@uic.edu>
2021-01-27 00:09:03 -0600

    dash

--- temp1.py
+++ temp1.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import pickle as p
+
+
+p_path = 'D:/uic/lab/data/naba/18_2_id_pep_dict.p'
+file_id_pep_dict = p.load(open(p_path,'rb'))
+
 
 # get time-series accumulated covearge for ECM proteins
 df = pd.read_csv('D:/uic/lab/data/naba/dash_info_new_1_20.csv')
@@ -19,14 +25,16 @@ for prot in protein_id_set[:2]:
     for idx, val in enumerate(index_18_2):
         index_list = index_18_2[:idx+1]
         print (index_list)
-        coverage = 0
+        pep_list = []
         for each_file in index_list:
             try:
 
-                coverage+=df[(df['protein_id']==prot)&(df['file_name']==each_file)]['coverage'].values[0]
-                print (coverage)
-            except IndexError:
+                pep_list+=list(file_id_pep_dict[each_file][prot])
+
+            except KeyError:
                 continue
+
+
         df_18_2_accumulated.loc[val,prot] = coverage
 
 print (df_18_2_accumulated)
