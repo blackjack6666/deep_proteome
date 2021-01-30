61e3af893c76bbdfc09137b547bf9b9ab039e712
Xinhao <xshao8@uic.edu>
2021-01-30 17:25:19 -0600

    proteome_cov

--- calculations_and_plot.py
+++ calculations_and_plot.py
@@ -81,11 +81,11 @@ def whole_proteome_cov(aho_result, seq_line):
     """
 
     total_seq_len = len(seq_line)
-    zero_line = np.zeros(len(seq_line))
+    zero_line = np.zeros(total_seq_len)
     for pos in aho_result:
         zero_line[pos[0]:pos[1] + 1] += 1
     non_zero_len = np.count_nonzero(zero_line)
-    print (non_zero_len)
+    # print (non_zero_len)
 
     return float(non_zero_len)/total_seq_len*100
 
