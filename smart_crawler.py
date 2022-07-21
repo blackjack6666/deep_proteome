1c81ebf4eb25585050d2a957f9ba90ced3443878
blackjack <xshao8@uic.edu>
2022-07-21 11:45:06 -0500

    bokeh graph for domain coverage and ptm occurance

--- smart_crawler.py
+++ smart_crawler.py
@@ -1,8 +1,6 @@
 """
 some function/features for MatrisomeDB2.0
 """
-
-
 from urllib.request import Request, urlopen
 import time
 import random
@@ -16,6 +14,13 @@ from multiprocessing_naive_algorithym import *
 from aho_corasick import automaton_trie,automaton_matching
 from protein_coverage import fasta_reader
 from tsv_reader import psm_reader
+from bokeh.models import HoverTool, ColumnDataSource, FactorRange, LinearColorMapper,ColorBar,BasicTicker,PrintfTickFormatter
+from bokeh.palettes import Spectral7
+from bokeh.transform import factor_cmap
+from bokeh.plotting import figure
+from bokeh.io import save, output_file, show
+from bokeh.embed import components
+from math import pi
 
 
 def get_smart_info(protein_list:list):
@@ -98,13 +103,13 @@ def ptm_map(psm_list,protein_dict):
     :param protein_dict:
     :return:
     """
-    peptide_psm_dict = defaultdict(set)  # {'PEPTIDE':{'P[100]EPTIDE','PEPTIDE'}}
     regex_pat = '\w{1}\[\d+\.?\d+\]'  # universal ptm pattern
 
     regex_set = set()
-
-    id_ptm_idx_dict = {}  # {protein_id:{ptm1:nonzero_index_array,ptm2:nonzero_index_array,...}}
     peptide_psm_dict = defaultdict(list)  # append all psm into a dictionary, {peptide:[psm1,psm2,...]}
+    id_ptm_idx_dict = {}  # {protein_id:{ptm1:nonzero_index_array,ptm2:nonzero_index_array,...}}
+    id_ptm_freq_dict = {}  # {protein_id:{ptm1:freq_array,ptm2:freq_array,...}}
+
     for each in psm_list:
         each_reg_sub = re.sub(regex_pat, my_replace, each)
         peptide_psm_dict[each_reg_sub].append(each)
@@ -112,8 +117,6 @@ def ptm_map(psm_list,protein_dict):
         if match:
             for ptm in match:
                 regex_set.add(ptm.replace('[','\[').replace(']','\]').replace('.','\.'))
-
-
 
     # aho mapping
     id_list, seq_list = extract_UNID_and_seq(protein_dict)
@@ -142,12 +145,16 @@ def ptm_map(psm_list,protein_dict):
                         for indx in PTM_index_clean:
                             ptm_index_line_dict[ptm][tp[0] + indx] += 1
 
+    # get ptm freq array and index
     for i in range(len(separtor_pos_array) - 1):
         id_ptm_idx_dict[id_list[i]] = {ptm:
             np.nonzero(ptm_index_line_dict[ptm][separtor_pos_array[i] + 1:separtor_pos_array[i + 1]])[0]
             for ptm in regex_set}
+        id_ptm_freq_dict[id_list[i]] = {ptm:
+            ptm_index_line_dict[ptm][separtor_pos_array[i] + 1:separtor_pos_array[i + 1]]
+            for ptm in regex_set}
 
-    return id_ptm_idx_dict
+    return id_ptm_idx_dict, id_ptm_freq_dict
 
 
 def plot_domain_coverage(prot_freq_dict,domain_pos_dict, protein_entry:str):
@@ -158,11 +165,6 @@ def plot_domain_coverage(prot_freq_dict,domain_pos_dict, protein_entry:str):
     :param protein_entry: target protein to plot
     :return:
     """
-    from bokeh.models import HoverTool, ColumnDataSource
-    from bokeh.palettes import Spectral7
-    from bokeh.transform import factor_cmap
-    from bokeh.plotting import figure
-    from bokeh.io import save, output_file, show
 
     freq_array = prot_freq_dict[protein_entry]
     domain_dict = domain_pos_dict[protein_entry]
@@ -214,18 +216,11 @@ def plot_domain_coverage2(prot_freq_dict,domain_pos_dict, protein_entry:str):
     :param protein_entry:
     :return:
     """
-    from bokeh.models import HoverTool, ColumnDataSource, FactorRange
-    from bokeh.palettes import Spectral7
-    from bokeh.transform import factor_cmap
-    from bokeh.plotting import figure
-    from bokeh.io import save, output_file, show
-    from math import pi
-
-
+    time_start = time.time()
     freq_array = prot_freq_dict[protein_entry]
     domain_dict = domain_pos_dict[protein_entry]
 
-    # a dataframe to store data
+    # a dataframe to store data, might not be needed
     df = pd.DataFrame(columns=['pos_start_end', 'sum_spec_count', 'domain_name'])
     idx = 0
     for each_domain in domain_dict:
@@ -257,7 +252,7 @@ def plot_domain_coverage2(prot_freq_dict,domain_pos_dict, protein_entry:str):
     TOOLTIPS = [
         ("domain", "@domain"),
         ("start end position", "@pos"),
-        ("normalized coverage", "$y"),
+        ("normalized coverage", "@y"),
     ]
 
     p = figure(x_range=FactorRange(*factors), plot_height=400, plot_width=len(factors)*40, tooltips=TOOLTIPS,
@@ -269,11 +264,75 @@ def plot_domain_coverage2(prot_freq_dict,domain_pos_dict, protein_entry:str):
     p.x_range.range_padding = 0.05
     p.xgrid.grid_line_color = None
     # p.xaxis.axis_label = 'whatever'
-    p.xaxis.major_label_text_font_size = "10pt"
-    p.xaxis.group_text_font_size = "15pt"
+    p.xaxis.major_label_text_font_size = "8pt"
+    p.xaxis.group_text_font_size = "10pt"
     # p.xaxis.major_label_text_font = 'Arial'
     p.xaxis.major_label_orientation = pi/2
-    show(p)
+    # show(p)
+    print (f'time used {time.time()-time_start}s')
+    # return the js script and div for html embedding
+    return components(p)
+
+
+def ptm_domain_htmap(ptm_map_result,domain_pos_dict,protein_entry:str):
+    """
+    use bokeh to generate a heatmap showing ptm coverage by domains
+    :param ptm_map_result: return from ptm_map, two returned values
+    :param domain_pos_dict: return from get_smart_info, SMART web crawler
+    :param protein_entry: target Uniprot ID
+    :return:
+    """
+    time_start = time.time()
+    id_ptm_indx_dict, id_ptm_freq_array_dict = ptm_map_result
+    ptm_indx_dict, ptm_freq_array_dict = id_ptm_indx_dict[protein_entry],id_ptm_freq_array_dict[protein_entry]
+    domain_dict = domain_pos_dict[protein_entry]
+
+    # create pd dataframe for plotting
+    x = [domain+'_'+str(tp[0])+'_'+str(tp[1]) for domain in domain_dict for tp in domain_dict[domain]]
+    y = [k.replace('\\','') for k in ptm_indx_dict.keys()]
+    # print (y)
+    df = pd.DataFrame(index=y,columns=x)
+    df.columns.name = 'domain_position'
+    df.index.name = 'PTMs'
+    for domain in domain_dict:
+        for tp in domain_dict[domain]:
+            start,end = tp[0], tp[1]
+            for each_ptm in ptm_freq_array_dict:
+                ptm_occur = np.count_nonzero(ptm_freq_array_dict[each_ptm][start-1:end])
+                df.at[each_ptm.replace('\\',''),domain+'_'+str(start)+'_'+str(end)] = ptm_occur
+
+    df_stack = pd.DataFrame(df.stack(),columns=['value']).reset_index() # domain and PTMs for each row
+
+    # plotting
+    colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+    mapper = LinearColorMapper(palette=colors, low=df_stack.value.min(), high=df_stack.value.max())
+
+    p = figure(x_range=x, y_range=y, plot_height=400, plot_width=len(x)*30,
+               tooltips=[('domain position and PTM', '@domain_position @PTMs'), ('PTM occurrence', '@value')],
+               y_axis_label="PTMs",
+               title=f'{protein_entry} PTM heatmap')
+
+    p.rect(x="domain_position", y="PTMs", width=1, height=1,
+           source=df_stack,
+           fill_color={'field': 'value', 'transform': mapper},
+           line_color=None)
+
+    p.grid.grid_line_color = None
+    p.axis.axis_line_color = None
+    p.axis.major_tick_line_color = None
+    p.axis.major_label_text_font_size = "10px"
+    p.axis.major_label_standoff = 0
+    p.xaxis.major_label_orientation = pi / 3
+
+    color_bar = ColorBar(color_mapper=mapper, major_label_text_font_size="7px",
+                         ticker=BasicTicker(desired_num_ticks=len(colors)),
+                         formatter=PrintfTickFormatter(format="%d"),
+                         label_standoff=6, border_line_color=None)
+    p.add_layout(color_bar, 'right')
+
+    # show(p)
+    print (f'time used {time.time()-time_start}s')
+    return components(p)
 
 
 def color_generator():
@@ -282,17 +341,52 @@ def color_generator():
     return '#%02X%02X%02X' % (r(),r(),r())
 
 
+def combine_bokeh(domain_bokeh_return, ptm_bokeh_return,html_out='test.html'):
+    """
+    generate the domain coverage bar graph and ptm domain heatmap in the same html
+    :param domain_bokeh_return: script, div from plot_domain_coverage2
+    :param ptm_bokeh_return: script, div from ptm_domain_htmap
+    :return:
+    """
+    # load bokeh js scripts and divs
+    coverage_script, coverage_div = domain_bokeh_return
+    ptm_script, ptm_div = ptm_bokeh_return
+
+    # print (coverage_script,coverage_div)
+    # read html template
+    html_template_f = open('F:/matrisomedb2.0/bokeh_html_template.html')
+    html_template = html_template_f.read()
+    html_template_f.close()
+
+    # write new html with js script and divs
+    new_html = html_template.replace('<!-- COPY/PASTE domain coverage SCRIPT HERE -->',coverage_script).\
+        replace('<!-- COPY/PASTE ptm SCRIPT HERE -->',ptm_script).replace('<!-- INSERT domain DIVS HERE -->',coverage_div)\
+        .replace('<!-- INSERT ptm DIVS HERE -->',ptm_div)
+
+    with open(html_out, 'w',newline='\n') as f_w:
+        f_w.write(new_html)
+
+    return new_html
+
+
 if __name__=='__main__':
     from tsv_reader import modified_peptide_from_psm
+    # SMART web crawler to extract domain info
     prot_list = ['Q8TER0','E9PWQ3','P11276']
     info_dict = get_smart_info(prot_list)
+
+    # peptide mapping
     psm_tsv = 'D:/data/Naba_deep_matrisome/07232021_secondsearch/SNED1_1080D/psm.tsv'
     psm_dict = psm_reader(psm_tsv)[0]
     protein_dict = fasta_reader('D:/data/Naba_deep_matrisome/uniprot-proteome_UP000000589_mouse_human_SNED1.fasta')
     protein_freq_dict = peptide_map(psm_dict,protein_dict)
-
-    plot_domain_coverage2(protein_freq_dict,info_dict,'P11276')
+    # domain coverage
+    domain_coverage_bokeh = plot_domain_coverage2(protein_freq_dict,info_dict,'Q8TER0')
 
     # main('https://smart.embl.de/smart/show_motifs.pl?ID=Q8TER0')
-    # psm_list = modified_peptide_from_psm(psm_tsv)
-    # print (ptm_map(psm_list,protein_dict)['P10493']['Q\\[111\\]'])
+    psm_list = modified_peptide_from_psm(psm_tsv)
+    ptm_map_result = ptm_map(psm_list,protein_dict)
+
+    ptm_domain_bokeh = ptm_domain_htmap(ptm_map_result,info_dict,'Q8TER0')
+    # combine domain coverage and ptm heatmaps in one html
+    combine_bokeh(domain_coverage_bokeh,ptm_domain_bokeh,html_out='F:/matrisomedb2.0/bokeh_test_Q8TER0.html')
